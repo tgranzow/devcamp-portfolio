5b675ef2a5bfcea0cac56d641161fbbc625c03ed
tnepper <Talia@MacBook-2.local>
2017-04-26 12:28:53 -0500

    Refactored strong params for portfolio items

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -77,5 +77,5 @@ end
     # Never trust parameters from the scary internet, only allow the white list through.
     def blog_params
       params.require(:blog).permit(:title, :body)
-    end
+  end
 end
